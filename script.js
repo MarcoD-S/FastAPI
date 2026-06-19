c2e2fe52577b84953490fc1824374b0200bdeda7
MarcoD-S <joaomarcoduarte149@gmail.com>
2026-06-19 16:23:10 -0300

    Criação de barra de pesquisa e nova orientação da pagina formato lista

--- script.js
+++ script.js
@@ -1,44 +1,82 @@
+let todosJogos = [];
+
+const botaoLayout = document.getElementById("trocarLayout");
+const container = document.getElementById("jogos");
+
+botaoLayout.addEventListener("click", () => {
+
+    container.classList.toggle("lista");
+
+    if(container.classList.contains("lista")){
+        botaoLayout.textContent = "🔲 Grade";
+    }else{
+        botaoLayout.textContent = "📋 Lista";
+    }
+
+});
+
+function renderizarJogos(jogos){
+
+    container.innerHTML = "";
+
+    jogos.forEach(jogo => {
+
+        container.innerHTML += `
+            <div class="card">
+
+                <img
+                    src="http://127.0.0.1:8000${jogo.imagem}"
+                    alt="${jogo.nome}"
+                >
+
+                <div class="card-content">
+
+                    <h3>${jogo.nome}</h3>
+
+                    <div class="info">
+                        <span>🎮 ${jogo.genero}</span>
+                        <span>📅 ${jogo.ano}</span>
+                        <span>⭐ ${jogo.nota ?? "N/A"}</span>
+                    </div>
+
+                    <p class="dev">
+                        Desenvolvedora: ${jogo.desenvolvedora}
+                    </p>
+
+                    <p class="descricao">
+                        ${jogo.descricao}
+                    </p>
+
+                </div>
+
+            </div>
+        `;
+    });
+}
+
 fetch("http://127.0.0.1:8000/jogos")
     .then(res => res.json())
     .then(jogos => {
 
-        const container = document.getElementById("jogos");
+        todosJogos = jogos;
 
-        jogos.forEach(jogo => {
-
-            container.innerHTML += `
-                <div class="card">
-
-                    <img
-                        src="http://127.0.0.1:8000${jogo.imagem}"
-                        alt="${jogo.nome}"
-                    >
-
-                    <div class="card-content">
-
-                        <h3>${jogo.nome}</h3>
-
-                        <div class="info">
-                            <span>🎮 ${jogo.genero}</span>
-                            <span>📅 ${jogo.ano}</span>
-                        </div>
-
-                        <p class="dev">
-                            Desenvolvedora: ${jogo.desenvolvedora}
-                        </p>
-
-                        <p class="descricao">
-                            ${jogo.descricao}
-                        </p>
-
-                    </div>
-
-                </div>
-            `;
-
-        });
+        renderizarJogos(jogos);
 
     })
     .catch(error => {
         console.error(error);
+    });
+
+document
+    .getElementById("pesquisa")
+    .addEventListener("input", e => {
+
+        const texto = e.target.value.toLowerCase();
+
+        const filtrados = todosJogos.filter(jogo =>
+            jogo.nome.toLowerCase().includes(texto)
+        );
+
+        renderizarJogos(filtrados);
+
     });
